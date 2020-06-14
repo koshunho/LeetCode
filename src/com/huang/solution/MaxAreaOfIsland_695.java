983da906bd27eb9a0d27ad91c783b7136529dd1c
koshunho <kosyuhon@fuji.waseda.jp>
2020-06-14 23:41:18 +0900

    695题

---
+++ src/com/huang/solution/MaxAreaOfIsland_695.java
@@ -0,0 +1,83 @@
+package com.huang.solution;
+/*695. 岛屿的最大面积
+* 给定一个包含了一些 0 和 1 的非空二维数组 grid 。
+
+一个 岛屿 是由一些相邻的 1 (代表土地) 构成的组合，这里的「相邻」要求两个 1 必须在水平或者竖直方向上相邻。你可以假设 grid 的四个边缘都被 0（代表水）包围着。
+
+找到给定的二维数组中最大的岛屿面积。(如果没有岛屿，则返回面积为 0 。)
+
+ 
+
+示例 1:
+
+[[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+对于上面这个给定矩阵应返回 6。注意答案不应该是 11 ，因为岛屿只能包含水平或垂直的四个方向的 1 。
+
+示例 2:
+
+[[0,0,0,0,0,0,0,0]]
+对于上面这个给定的矩阵, 返回 0。
+
+ 
+
+注意: 给定的矩阵grid 的长度和宽度都不超过 50。
+。*/
+
+// 经典DFS。关键点：搜索过的地方要置0
+public class MaxAreaOfIsland_695 {
+
+    int [] dx = new int []{0,1,0,-1};
+    int [] dy = new int []{-1,0,1,0};
+
+    public int maxAreaOfIsland(int[][] grid) {
+        if(grid.length == 0) return 0;
+        int max = 0;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == 1){
+                    max = Math.max(dfs(grid,i,j), max);
+                }
+            }
+        }
+        return max;
+    }
+
+    public int dfs(int[][] grid, int i, int j){
+        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0) return 0;
+
+        int count = 1;
+
+        grid[i][j] = 0; //最关键的一点
+
+        for(int x = 0; x < 4; x++){
+            count += dfs(grid, i+dx[x], j+dy[x]);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int [][] grid = new int [][]{
+                {0,0,1,0,0,0,0,1,0,0,0,0,0},
+                {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                {0,1,1,0,1,0,0,0,0,0,0,0,0},
+                {0,1,0,0,1,1,0,0,1,0,1,0,0},
+                {0,1,0,0,1,1,0,0,1,1,1,0,0},
+                {0,0,0,0,0,0,0,0,0,0,1,0,0},
+                {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                {0,0,0,0,0,0,0,1,1,0,0,0,0}};
+
+        int ret = new MaxAreaOfIsland_695().maxAreaOfIsland(grid);
+
+        String out = String.valueOf(ret);
+
+        System.out.println(out);
+    }
+}
