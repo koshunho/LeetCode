bc2b2659d097ca2477e4e88b9812e6ac738dbb85
koshunho <kosyuhon@fuji.waseda.jp>
2020-06-16 03:37:25 +0900

    49题

---
+++ src/com/huang/solution/MaxArea_11.java
@@ -0,0 +1,43 @@
+package com.huang.solution;
+/*11. 盛最多水的容器
+* 给你 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+
+说明：你不能倾斜容器，且 n 的值至少为 2。
+
+ 
+
+
+
+图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+
+ 
+
+示例：
+
+输入：[1,8,6,2,5,4,8,3,7]
+输出：49
+*/
+
+// 由于不是数学家不能给出很好的解释。说说自己理解
+// 两个木板之间的面积，取决于最短的那块木板。
+// 然后我舍弃掉这块较短的木板，试试下一块看看，说不定比当前的高
+public class MaxArea_11 {
+    public int maxArea(int[] height) {
+        int len = height.length;
+        int max = 0;
+        for(int i = 0,j = len-1; i < j;){
+            int minHeight = height[i] < height[j] ? height[i++] : height[j--];
+            int square = minHeight * (j - i + 1);
+            max = Math.max(max, square);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int [] heights = new int []{1,8,6,2,5,4,8,3,7};
+
+        int ret = new MaxArea_11().maxArea(heights);
+
+        System.out.println(ret);
+    }
+}
